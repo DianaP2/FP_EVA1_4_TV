5df14b1454fcf0b16936cc1fcf6671c2028855b3
DianaP2 <L24550326@outlook.com>
2024-08-28 21:57:16 -0600

    Add files via upload

---
+++ EVA1_4_TV/src/eva1_4_tv/EVA1_4_TV.java
@@ -0,0 +1,37 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package eva1_4_tv;
+
+/**
+ *
+ * @author melan
+ */
+public class EVA1_4_TV {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        String marca;
+        String modelo;
+        int pulg;
+        double precio;
+        String tecno;
+        
+        marca="LG";
+        modelo="LG";
+        pulg=47;
+        precio=10000;
+        tecno="LED";
+        
+        System.out.println(marca);
+        System.out.println(modelo);
+        System.out.println(pulg);
+        System.out.println(precio);
+        System.out.println(tecno);
+    }
+    
+}
